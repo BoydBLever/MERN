efe6cb70e74ce34a886e97b29d7c5ee2bece413d
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-22 21:34:09 -0800

    update

--- Authors/client/src/components/Update.jsx
+++ Authors/client/src/components/Update.jsx
@@ -1,20 +1,45 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 //update takes two axios calls
 const Update = (props) => {
   const navigate = useNavigate()
 
+  //grab the id from the params
+  const { id } = useParams()
+
   const [name, setName] = useState("")
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/authors/${id}`)
+      .then(res => {
+        console.log(res.data);
+        setName(res.data.name);
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const updateAuthor = (e) => {
+    e.preventDefault();
+
+    axios.put(`http://localhost:8000/api/authors/${id}`, {
+      name
+    })
+    .then(res => {
+      console.log(res.data)
+      navigate("/authors");
+    })
+    .catch(err => console.log(err))
+  }
 
   return (
     <div>
-      <form >
-                Name: <input onChange={e => setName(e.target.value)} value={name} /> <br />
-                <button>Submit</button> &nbsp;
-                <button>Cancel</button>
-            </form>
+      <form onSubmit={updateAuthor} >
+        Name: <input onChange={e => setName(e.target.value)} value={name} /> <br />
+        <button>Submit</button> &nbsp;
+        <button>Cancel</button>
+      </form>
     </div>
   )
 }
