70703586daa7349691bc81410d1dc2ece40e6099
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-23 18:41:15 -0800

    update

--- TeamManager/client/src/App.js
+++ TeamManager/client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {Link, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>⚾️ Team Manager 🏟️ 🧢</h1>
+      <Link to="/players">List</Link> &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link to="/new">Add Player</Link>
+      <hr/>
     </div>
   );
 }
