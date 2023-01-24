4d9d162b4bca9b261ff20e75b861d7d9ace6e399
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-23 18:53:16 -0800

    update

--- TeamManager/client/src/App.js
+++ TeamManager/client/src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import {Link, Routes, Route} from 'react-router-dom';
+import { Link, Routes, Route, Navigate } from 'react-router-dom';
+import Main from './components/Main';
+import Create from './components/Create';
 
 function App() {
   return (
     <div className="App">
       <h1>⚾️ Team Manager 🏟️ 🧢</h1>
-      <Link to="/players">List</Link> &nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/new">Add Player</Link>
-      <hr/>
+      <Link to="/players/list">List</Link> &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link to="/players/addplayer">Add Player</Link>
+      <hr />
+      <Routes>
+        {/* MAIN ROUTE SHOWS ALL PLAYERS */}
+        <Route path="/players/list" element={<Main />} />
+
+        {/* ADD A NEW PLAYER */}
+        <Route path="/players/addplayer" element={<Create />} />
+
+        {/* REDIRECT */}
+        <Route path="*" element={<Navigate to="/players/list" replace />} />
+
+      </Routes>
     </div>
   );
 }
