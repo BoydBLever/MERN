70607e6bde27f4b8a7fcfcfe63bcc63d0799e348
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-23 20:43:00 -0800

    update

--- TeamManager/client/src/components/Status.jsx
+++ TeamManager/client/src/components/Status.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import playerStyle from './main.module.css';
 
 const Status = (props) => {
-  return (
-    <div>
-        <h1>Game 1</h1>
-        <h1>Game 2</h1>
-        <h1>Game 3</h1>
-    </div>
-  )
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:1337/api/players")
+            .then(res => {
+                console.log(res.data);
+                setPlayers(res.data)
+
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div>
+            <h1>List</h1>
+            {
+                players.map((onePlayer, index) => {
+                    return (
+                        <div key={onePlayer._id} className={playerStyle.player2}>
+                            <h1>{onePlayer.name}</h1>
+                            <button>Playing</button>
+                            <button>Not Playing</button>
+                            <button>Undecided</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Status
