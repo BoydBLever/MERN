159a4647498e37f2b08ab13395e35f202ccd8337
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-23 19:52:46 -0800

    update

--- TeamManager/client/src/components/Create.jsx
+++ TeamManager/client/src/components/Create.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Create = (props) => {
-  return (
-    <div>Create</div>
-  )
+
+    const navigate = useNavigate()
+
+    const [name, setName] = useState("")
+    const [position, setPosition] = useState("")
+
+    const [errors, setErrors] = useState([]);
+
+    const createPlayer = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:8000/api/players", {
+            name, position
+        })
+        .then(res => {
+            console.log(res.data);
+            navigate("/players");
+        })
+        .catch(err => {
+            console.log(err.response.data);
+            const errorResponse = err.response.data.errors;
+            const errorArr=[];
+            for (const key of Object.keys(errorResponse)){
+                errorArr.push(errorResponse[key].message)
+            }
+            setErrors(errorArr);
+        })
+    }
+
+    return (
+        <div>
+            {errors.map((err, index) => <p style={{color: "red"}} key={index}>{err}</p>)}
+            <form onSubmit={createPlayer} >
+                Name: <input onChange={e => setName(e.target.value)} value={name} /> <br />
+                Position: <input onChange={e => setPosition(e.target.value)} value={position} /> <br />
+                <button>Submit</button> &nbsp;
+            </form>
+        </div>
+    )
 }
 
 export default Create
