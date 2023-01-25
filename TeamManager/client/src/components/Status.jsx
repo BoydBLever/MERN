8cedb89834b207f9a6477874039673b94cba2f60
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-24 17:21:11 -0800

    update

--- TeamManager/client/src/components/Status.jsx
+++ TeamManager/client/src/components/Status.jsx
@@ -27,9 +27,9 @@ const Status = (props) => {
     
     return (
         <div>
-            <h1>Player Status Game 1</h1>
-            <Link to="/status/game/1">Game 1</Link> 
-            <Link to="/status/game/2">Game 2</Link> 
+            <h1>Player Status - Game 1</h1>
+            <Link to="/status/game/1">Game 1 |</Link> &nbsp;
+            <Link to="/status/game/2">Game 2 |</Link> &nbsp;
             <Link to="/status/game/3">Game 3</Link> 
             {
             //if players is not null, then you map through the players array.
