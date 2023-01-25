cb3440459975be1d29b4e32a1b7515c8207b55e6
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-25 10:30:56 -0800

    update

--- TeamManager/client/src/components/Game1.jsx
+++ TeamManager/client/src/components/Game1.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import playerStyle from './main.module.css';
+import { Link } from 'react-router-dom';
+import './game.css';
 
 
 const Game1 = (props) => {
     const [players, setPlayers] = useState(null);
-    const [game, setGame] = useState("undecided");
+    // const [game, setGame] = useState("undecided");
+    const [refresh, setRefresh] = useState(false);
 
     useEffect(() => {
         axios.get("http://localhost:1337/api/players")
@@ -14,16 +17,19 @@ const Game1 = (props) => {
                 setPlayers(res.data)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [refresh])
 
-    const clickHandler = (e, gameID) => {
-        e.preventDefault();
+    const clickHandler = (game, id) => {
         //update the player.game to playing, not playing or undecided
-        const gameUpdate = {
-            game: 'name'
-        }
-        setGame('name');
-    }
+        let onePlayer = players.filter((p) => p._id === id)
+        onePlayer.game1 = game;
+        console.log(onePlayer.game1);
+        axios.put(`http://localhost:1337/api/players/${id}`, {...onePlayer,game1:game})
+        .then( res => {
+            console.log(res);
+            setRefresh(!refresh);
+        })
+        .catch(err => console.log(err))
     }
 
     return (
@@ -37,9 +43,9 @@ const Game1 = (props) => {
                     return (
                         <div key={onePlayer._id} className={playerStyle.player2}>
                             <h1>{onePlayer.name}</h1>
-                            <button name='playing' className={onePlayer.game === playing ? 'green' : ''} onClick={clickHandler('playing')}>Playing</button>
-                            <button name='not playing' className={onePlayer.game === notplaying ? 'red' : ''} onClick={clickHandler('not playing')}>Not Playing</button>
-                            <button name='undecided' className={onePlayer.game === undecided ? 'yellow' : ''} onClick={clickHandler('undecided')}>Playing</button>
+                            <button name='playing' className={onePlayer.game1 === "playing" ? 'green' : ''} onClick={() => {clickHandler('playing', onePlayer._id)}}>Playing</button>
+                            <button name='not playing' className={onePlayer.game1 === "not playing" ? 'red' : ''} onClick={() => {clickHandler('not playing', onePlayer._id)}}>Not Playing</button>
+                            <button name='undecided' className={onePlayer.game1 === "undecided" ? 'yellow' : ''} onClick={() => {clickHandler('undecided', onePlayer._id)}}>Playing</button>
                         </div>
                     )
                 })
